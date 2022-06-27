aa79507cd60b8a0f5ca9cdd9cb06167174de4a6d
Blaze Scott <blazeascott@gmail.com>
2022-06-27 16:49:45 -0500

    Cleanup and transition implimented
    Still working on the transition of the title and button

--- app.js
+++ app.js
@@ -1,59 +1,120 @@
+// todo: rename resources to something more applicable
+
 import Movie from './scripts/movie-class.js';
+import {
+    searchApiStr,
+    movieDataApiStr,
+    getFindPageValues,
+    getWatchListValues,
+    getNoResultsHtml,
+    getSearchDefaultHtml,
+} from './scripts/resources.js';
+
+let watchList = []
+
+const validateData = (movieData) => {
+    let validData = [];
+    let pass = false;
+    for (let i of movieData) {
+        // todo: do I need the key here?
+        for (let [key, value] of Object.entries(i)) {
+            if (value === 'N/A') {
+                pass = true;
+                continue;
+            }
+        }
+        if (pass) continue;
+        validData.push(i);
+    }
+    return validData;
+};
 
 const getSearchResults = async (title) => {
-    const resp = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=919de648&s=${title}&type=movie`)
-    const searchData = await resp.json()
-    return searchData
-}
+    const resp = await fetch(searchApiStr(title));
+    const searchData = await resp.json();
+    // todo: incorporate exception handling around here
+    if (searchData.Error) {
+        return false;
+    }
+    const validSearch = validateData(searchData.Search);
+    return validSearch;
+};
 
 const getMovieData = async (title, year) => {
-    const resp = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=919de648&t=${title}&y=${year}&type=movie`)
-    const movieData = await resp.json()
-    return movieData
-}
+    const resp = await fetch(movieDataApiStr(title, year));
+    const movieData = await resp.json();
+    return movieData;
+};
 
 const makeMovieObject = (movieData) => {
     return new Movie(
         movieData.Title,
-        //todo figure out the ratings problem
-        // movieData.Ratings[0].Value,
+        movieData.Ratings[0].Value,
         movieData.Runtime,
         movieData.Genre,
         movieData.Plot,
         movieData.Poster
-    )
-}
+    );
+};
 
 const getMovieObjects = async (searchData) => {
-    let movieObjects = []
+    let movieObjects = [];
     for (let i = 0; i < searchData.length; i++) {
-        const movieData = await getMovieData(searchData[i].Title, searchData[i].Year)
-        const newMovie = makeMovieObject(movieData)
-        movieObjects.push(newMovie)
-        console.log(newMovie)
+        const movieData = await getMovieData(
+            searchData[i].Title,
+            searchData[i].Year
+        );
+        const newMovie = makeMovieObject(movieData);
+        movieObjects.push(newMovie);
     }
-    return movieObjects
-}
-
-const displayResults = (placement, movieObjects) => {
-    placement.innerHTML = ''
-    for (let i of movieObjects) {
-        placement.insertAdjacentHTML('beforeend', i.getCardHtml())
-    }
-}
+    return movieObjects;
+};
 
 const searchWrapper = async (placement, title) => {
-    const searchData = await getSearchResults(title)
-    const movieObjects = await getMovieObjects(searchData.Search)
-    displayResults(placement, movieObjects)
-}
+    const searchData = await getSearchResults(title);
+    placement.innerHTML = '';
+    if (searchData) {
+        const movieObjects = await getMovieObjects(searchData);
+        for (let i of movieObjects) {
+            placement.insertAdjacentHTML('beforeend', i.getCardHtml());
+        }
+        return;
+    }
+    placement.innerHTML = getNoResultsHtml()
+};
 
-const mainContent = document.getElementById('main-content')
+const mainContent = document.getElementById('main-content');
+const navBtn = document.getElementById('navigation-btn')
 
-searchWrapper(mainContent, 'rango')
+// searchWrapper(mainContent, 'rango');
 
 //! working area //
 
-function validateData(movieData) {
-    
+const flipPage = (pageName) => {
+    const title = document.getElementById('title')
+    const searchBarEl = document.getElementById('search-bar')
+
+    const find = getFindPageValues()
+    const watch = getWatchListValues()
+
+    mainContent.innerHTML = ''
+
+    if (pageName !== 'find') {
+        title.textContent = find.title
+        navBtn.textContent = find.navBtn
+        navBtn.dataset.page = 'find'
+        searchBarEl.classList.remove('hidden')
+        mainContent.innerHTML = getSearchDefaultHtml()
+        return;
+    }
+    title.textContent = watch.title
+    navBtn.textContent = watch.navBtn
+    navBtn.dataset.page = 'watch'
+    searchBarEl.classList.add('hidden')
+    mainContent.innerHTML = watchList.join('')
 }
+
+navBtn.addEventListener('click', (e) => {
+    // todo Think of a better way to differentiat these two pages
+    flipPage(e.target.dataset.page)
+})
--- scripts/movie-class.js
+++ scripts/movie-class.js
@@ -1,12 +1,20 @@
-class Movie{
-    constructor(title,  runTime, genre, description, poster, saved = false) {
-        this.title = title
-        // this.rating = ratings
-        this.runTime = runTime
-        this.genre = genre
-        this.description = description
-        this.poster = poster
-        this.saved = saved
+class Movie {
+    constructor(
+        title,
+        ratings,
+        runTime,
+        genre,
+        description,
+        poster,
+        saved = false
+    ) {
+        this.title = title;
+        this.rating = ratings;
+        this.runTime = runTime;
+        this.genre = genre;
+        this.description = description;
+        this.poster = poster;
+        this.saved = saved;
     }
     getCardHtml() {
         return `
@@ -17,6 +25,7 @@ class Movie{
                         <p class="rating flex">
                             <img    src="./assets/star-icon.png" 
                                     alt="star icon">
+                            <span class="rating-num">${this.rating}</span>
                         </p>
                     </div>
                     <div class="movie-data flex">
@@ -28,56 +37,22 @@ class Movie{
                 </div>
                 <img class="movie-img" src="${this.poster}" alt="movie poster">
             </div>
-        
-        `
+        `;
     }
     getWatchListHtml() {
-        let symbol = "./assets/plus-icon.png"
-        let word = "Add"
+        let symbol = './assets/plus-icon.png';
+        let word = 'Add';
         if (this.saved) {
-            symbol = "./assets/minus-icon.png"
-            word = "Remove"
+            symbol = './assets/minus-icon.png';
+            word = 'Remove';
         }
         return `
-            <button class="watch-list-btn flex" data-watch-list-btn="${this.title}">
+            <button class="watch-list-btn flex" data-watch-list-btn="${this.title} ${this.runTime}">
                 <img src=${symbol} alt="plus symbol"> 
                 ${word} to watchlist
             </button> 
-        `
+        `;
     }
-
-
 }
 
 export default Movie;
-
-
-// Actors: "Harrison Ford, Rutger Hauer, Sean Young"
-// Awards: "Nominated for 2 Oscars. 12 wins & 19 nominations total"
-// BoxOffice: "$32,914,489"
-// Country: "United States"
-// DVD: "30 Oct 2001"
-// Director: "Ridley Scott"
-// Genre: "Action, Drama, Sci-Fi"
-// Language: "Low German, English, German, Cantonese, Japanese, Hungarian, Arabic, Korean"
-// Metascore: "84"
-// Plot: "A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator."
-// Poster: "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-// Production: "N/A"
-// Rated: "R"
-// Ratings: (3) [{…}, {…}, {…}]
-// 0: {Source: 'Internet Movie Database', Value: '8.1/10'}
-// 1: {Source: 'Rotten Tomatoes', Value: '89%'}
-// 2: {Source: 'Metacritic', Value: '84/100'}
-// length: 3
-// Released: "25 Jun 1982"
-// Response: "True"
-// Runtime: "117 min"
-// Title: "Blade Runner"
-// Type: "movie"
-// Website: "N/A"
-// Writer: "Hampton Fancher, David Webb Peoples, Philip K. Dick"
-// Year: "1982"
-// imdbID: "tt0083658"
-// imdbRating: "8.1"
-// imdbVotes: "750,069"
